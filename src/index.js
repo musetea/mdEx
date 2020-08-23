d002e24209f1326faa1ab4fc5f112b9ac6fddc32
musetea <dev0019@naver.com>
2020-08-23 23:24:09 +0900

    커밋

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import * as lib from './lib';
-import { has } from '../lib/markdown-it';
+import hljs from 'highlight.js';
+console.log(hljs);
+require("codemirror/mode/javascript/javascript");
+require("codemirror/addon/edit/closebrackets");
+const CodeMirror = require("codemirror");
+//require("codemirror/");
+require('../lib/spell-checker.min');
+require('../lib/rawinflate');
+require('../lib/rawdeflate');
+require('../lib/sweetalert.min');
+
 
 const URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 navigator.saveBlob = navigator.saveBlob || navigator.msSaveBlob || navigator.mozSaveBlob || navigator.webkitSaveBlob;
@@ -10,21 +19,28 @@ const languageOverrides = {
     html: 'xml'
 };
 
-const md = lib.markdownit({
-    html: true,
-    highlight: function(code, lang) {
-        if (languageOverrides[lang]) lang = languageOverrides[lang];
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                return hljs.highlight(lang, code).value;
-            } catch (e) { console.log(e); }
+const md = require('markdown-it')({
+        html: true,
+        highlight: function(code, lang) {
+            if (languageOverrides[lang]) lang = languageOverrides[lang];
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, code).value;
+                } catch (e) { console.log(e); }
+            }
+            return '';
         }
-        return '';
-    }
-})
-.use(lib.markdownitFootnote);
+    })
+    .use(require('markdown-it-footnote'));
+console.log(md);
 
+/**
+ * 
+ * @param {*} e 
+ */
 const update = (e) => {
+    console.log('udate');
+
     setOutput(e.getValue());
 
     
@@ -42,7 +58,7 @@ const update = (e) => {
 }
 
 
-const editor = lib.CodeMirror.fromTextArea(document.getElementById('code'), {
+const editor = CodeMirror.fromTextArea(document.getElementById('code'), {
     mode: "spell-checker",
     backdrop: "gfm",
     lineNumbers: false,
@@ -107,22 +123,22 @@ const run = () => {
     }else if(localStorage.getItem('content')){
         editor.setValue(localStorage.getItem('content'));
     }
-
+    update(editor);
     editor.focus();
     document.getElementById('fileInput').addEventListener('change', openFile, false);
 };
 
-window.addEventListener("beforeunload", (e) => {
+// window.addEventListener("beforeunload", (e) => {
 
-    if (!editor.getValue() || editor.getValue() == localStorage.getItem('content')) {
-        return;
-    }
+//     if (!editor.getValue() || editor.getValue() == localStorage.getItem('content')) {
+//         return;
+//     }
 
-    let confirmationMessage = 'It looks like you have been editing something. '
-                            + 'If you leave before saving, your changes will be lost.';
-    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-});
+//     let confirmationMessage = 'It looks like you have been editing something. '
+//                             + 'If you leave before saving, your changes will be lost.';
+//     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+//     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+// });
 
 
 run();
